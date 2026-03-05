20446c308ea51d3fc70d8d82be67a98228c58c9a
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-03-05 22:24:22 +0300

    Update 02. CDC setup help.sql

--- 30. Change data capture/02. CDC setup help.sql
+++ 30. Change data capture/02. CDC setup help.sql
@@ -1,3 +1,15 @@
 USE Production_DB;
 
 SELECT [name], object_id, is_tracked_by_cdc from sys.tables; -- CDC enabled tables
+
+EXECUTE sys.sp_cdc_help_change_data_capture; -- CDC setup for tables help
+
+
+
+
+
+
+
+
+
+
