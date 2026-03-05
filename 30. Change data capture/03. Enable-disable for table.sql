3fabc025363f367b05a2a7f77196b7f128e68676
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-03-05 22:49:25 +0300

    Update 03. Enable-disable for table.sql

--- 30. Change data capture/03. Enable-disable for table.sql
+++ 30. Change data capture/03. Enable-disable for table.sql
@@ -15,3 +15,13 @@ EXEC sys.sp_cdc_enable_table -- CDC enable second copy for table
     @supports_net_changes = 1
 ;
 
+EXEC sys.sp_cdc_disable_table -- CDC disable for table
+    @source_schema = 'dbo',
+    @source_name = 'Test_1',
+	@capture_instance = 'dbo_Test_1_new'
+;
+
+
+
+
+
