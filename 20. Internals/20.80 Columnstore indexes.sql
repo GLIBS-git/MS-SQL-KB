19710052640a96e8938b5018368a5a4fc627310f
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-03-23 00:40:19 +0300

    Update 20.80 Columnstore indexes.sql

--- 20. Internals/20.80 Columnstore indexes.sql
+++ 20. Internals/20.80 Columnstore indexes.sql
@@ -1,5 +1,5 @@
-select * from sys.column_store_dictionaries;
-
 select * from sys.column_store_row_groups;
+
+select * from sys.column_store_dictionaries;
 
 select * from sys.column_store_segments;
