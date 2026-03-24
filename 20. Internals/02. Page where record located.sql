fdf984aaef2fc64cf9a84c27c8880ca9e29bd85b
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-03-25 01:44:10 +0300

    Update 02. Page where record located.sql

--- 20. Internals/02. Page where record located.sql
+++ 20. Internals/02. Page where record located.sql
@@ -1,6 +1,7 @@
 --USE Production_DB_name; -- Database dependent
 
-select top 100 B.*, '--------' as [--------], A.* from Some_table_name A -- with (nolock) -- Displays file number, page number and slot where actual record is located
+select top 100 B.*, '--------' as [--------], A.* -- Displays file number, page number and slot where actual record is located
+from Some_table_name A -- with (nolock) 
 outer apply sys.fn_PhysLocCracker(%%physloc%%) B
 --where -- Optional condition
 --order by -- Sorting if necessary
