44805ef5e3ba5344649a96a922d96e33cf5089f2
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-03-25 12:17:36 +0300

    Update and rename 02. Page where record located.sql to 02. How to find page where record stored.sql

--- 20. Internals/02. How to find page where record stored.sql
+++ 20. Internals/02. How to find page where record stored.sql
@@ -1,8 +1,11 @@
 --USE Production_DB_name; -- Database dependent
 
-select top 100 B.*, '--------' as [--------], A.* -- Displays file number, page number and slot where actual record is located
+select top 100 B.*, '--------' as [--------], A.* -- Displays file number, page number and slot where record is stored
 from Some_table_name A -- with (nolock) 
 outer apply sys.fn_PhysLocCracker(%%physloc%%) B
---where -- Optional condition
+--where -- Some condition
 --order by -- Sorting if necessary
 ;
+
+
+
