7be23d90fe2a2ca4295c8f2e9eda8f9e3edeeb51
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-04-17 20:13:34 +0300

    Create 01. Index statistics.sql

---
+++ 10./Tunning/01. Index statistics.sql
@@ -0,0 +1,49 @@
+select -- Index statistics
+	DB_NAME(A.database_id) as [Database]
+	, OBJECT_NAME(A.object_id, A.database_id) as [Table]
+	, (select top 1 name from sys.indexes where object_id = A.object_id and index_id = A.index_id) as [Index]
+	, (select top 1 type_desc from sys.indexes where object_id = A.object_id and index_id = A.index_id) as [Index type]
+	, A.user_seeks
+	, A.user_scans
+	, A.user_lookups
+	, A.user_updates
+	, A.last_user_seek
+	, A.last_user_scan
+	, A.last_user_lookup
+	, A.last_user_update
+	, Z.Field_list as [Index_key_fields]
+	, Y.Field_list as [Index_include_fields]
+	--, A.database_id as [DataBase ID]
+	--, A.object_id as [Object ID]
+	--, A.index_id as [Index ID]
+	--, '-----' as [-----]
+	--, A.*
+from SYS.DM_DB_INDEX_USAGE_STATS A
+outer apply 
+(
+	select 
+	(
+		select cast(Y.name as nvarchar) + ',' as 'data()' from sys.index_columns X
+		inner join sys.columns Y on Y.object_id = A.object_id and Y.column_id = X.column_id
+		where X.object_id = A.object_id and X.index_id = A.index_id and X.is_included_column = 0
+		order by X.key_ordinal
+		for xml path('')
+	) as [Field_list]
+) Z
+outer apply 
+(
+	select 
+	(
+		select cast(Y.name as nvarchar) + ',' as 'data()' from sys.index_columns X
+		inner join sys.columns Y on Y.object_id = A.object_id and Y.column_id = X.column_id
+		where X.object_id = A.object_id and X.index_id = A.index_id and X.is_included_column = 1
+		order by X.key_ordinal
+		for xml path('')
+	) as [Field_list]
+) Y
+where A.object_id = OBJECT_ID('Some_table_name')
+and A.database_id = DB_ID('Production_DB_name')
+order by A.database_id, A.object_id, A.index_id
+;
+
+
