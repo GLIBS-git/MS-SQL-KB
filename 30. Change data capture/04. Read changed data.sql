e538e9101b1b970244893b7116fb62d8fc44150e
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-05-11 08:05:19 +0300

    Update 04. Read changed data.sql

--- 30. Change data capture/04. Read changed data.sql
+++ 30. Change data capture/04. Read changed data.sql
@@ -1,6 +1,7 @@
 --USE Production_DB_name; -- Database dependent
 
 select * from cdc.dbo_Test_1_CT; -- Read CDC data changes (only for investigating). [More description](/Help/04.%20Read%20changed%20data.md)
+/Help/04.%20Read%20changed%20data.md
 
 SELECT * FROM cdc.fn_cdc_get_all_changes_dbo_Test_1(0x00000293000001B80052, 0x0000029400000CE80001, 'all') as A; -- CDC read data changes (officially recommended)
 SELECT * FROM cdc.fn_cdc_get_all_changes_dbo_Test_1(0x00000293000001B80052, 0x0000029400000CE80001, 'all update old') as A;
