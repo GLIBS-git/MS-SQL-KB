fd9ce908b38c44a4459bed7aaa379c27eb84444f
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-05-16 17:26:33 +0300

    Create 01. Index operations.sql

---
+++ 03. Maintenance/01. Index operations.sql
@@ -0,0 +1,19 @@
+--USE Production_DB_name; -- Database dependent
+
+CREATE INDEX Index_name ON Table_name (Field_1,Field_1,...) WITH (ONLINE = OFF, SORT_IN_TEMPDB = ON, MAXDOP = 6);
+CREATE UNIQUE INDEX Index_name ON Table_name (Field_1,Field_1,...) with (ONLINE = OFF, sort_in_tempdb = on, maxdop = 6);
+CREATE CLUSTERED INDEX Index_name ON Table_name (Field_1,Field_1,...) WITH (ONLINE = OFF, SORT_IN_TEMPDB = ON, MAXDOP = 6);
+ALTER TABLE Table_name ADD CONSTRAINT Index_name PRIMARY KEY NONCLUSTERED (Field_1,Field_1,...) WITH (ONLINE = OFF, SORT_IN_TEMPDB = ON, MAXDOP = 6);
+
+ALTER index [Index_name] on [Table_name] rebuild WITH (sort_in_tempdb = on, maxdop = 3, ONLINE = OFF);
+
+DROP INDEX [Index_name] ON [dbo].[Table_name] WITH (ONLINE = OFF);
+ALTER TABLE [dbo].[Table_name] DROP CONSTRAINT [Index_name] WITH (ONLINE = OFF);
+
+
+
+
+
+
+
+
