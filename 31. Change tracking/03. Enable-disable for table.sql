a59bb01929776c0e1581c4eb4d4c96498aa3ee3d
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-05-18 14:56:05 +0300

    Update 03. Enable-disable for table.sql

--- 31. Change tracking/03. Enable-disable for table.sql
+++ 31. Change tracking/03. Enable-disable for table.sql
@@ -1,4 +1,4 @@
---USE Production_DB_name; -- Database dependent
+--USE Production_DB_name; -- Database dependent!
 
 ALTER TABLE [Some_table_name] ENABLE CHANGE_TRACKING WITH (TRACK_COLUMNS_UPDATED = ON); -- Enable change tracking for table
 
