a66677f826725c82c83d7bc2a723bdf1193b7725
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-05-18 15:55:44 +0300

    Update 04. Read changed data.sql

--- 31. Change tracking/04. Read changed data.sql
+++ 31. Change tracking/04. Read changed data.sql
@@ -1,5 +1,10 @@
 --USE Production_DB_name; -- Database dependent!
 
-select CHANGE_TRACKING_CURRENT_VERSION() as Track_version; -- Change tracking
+select CHANGE_TRACKING_CURRENT_VERSION() as Track_version; -- Current change tracking version
+
+select * from CHANGETABLE (CHANGES [Some_table_name], 0) as A; -- Read change trachikg data for table
+
+
+
 
 
