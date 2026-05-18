36d4e4a0cffc18887ceeacd1bde8df57b9f247ff
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-05-18 14:37:48 +0300

    Update 01. Enable-disable for DB.sql

--- 31. Change tracking/01. Enable-disable for DB.sql
+++ 31. Change tracking/01. Enable-disable for DB.sql
@@ -1,4 +1,5 @@
 ALTER DATABASE [Some_DB_name] SET CHANGE_TRACKING = ON (CHANGE_RETENTION = 2 DAYS, AUTO_CLEANUP = ON); -- Enable change tracking for DB
+ALTER DATABASE [Some_DB_name] SET CHANGE_TRACKING = ON; -- Enable change tracking for DB with default cleanup setup
 
 
 
