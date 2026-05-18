6ecf6a82dc2c3b24d0bb58caed5d5f45e3671efe
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-05-18 18:20:10 +0300

    Update 04. Read changed data.sql

--- 31. Change tracking/04. Read changed data.sql
+++ 31. Change tracking/04. Read changed data.sql
@@ -2,7 +2,9 @@
 
 select CHANGE_TRACKING_CURRENT_VERSION() as Track_version; -- Current change tracking version
 
-select * from CHANGETABLE (CHANGES [Some_table_name], 0) as A; -- Read change trachikg data for table
+select * from CHANGETABLE (CHANGES [Some_table_name], 0) as A -- Read change trachikg data for table
+--where CHANGE_TRACKING_IS_COLUMN_IN_MASK(COLUMNPROPERTY(OBJECT_ID('Table_name'), 'Column_name', 'ColumnId'), A.SYS_CHANGE_COLUMNS) = 1 -- Only changes for some column
+;
 
 
 
