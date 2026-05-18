f953450193a7bee3b416360478176953f3015c5d
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-05-18 17:53:17 +0300

    Update 02. CT setup help.sql

--- 31. Change tracking/02. CT setup help.sql
+++ 31. Change tracking/02. CT setup help.sql
@@ -1,10 +1,19 @@
 SELECT DB_NAME(database_id) AS DatabaseName, is_auto_cleanup_on, retention_period, retention_period_units_desc FROM sys.change_tracking_databases; -- Change thacking enabled databases
 
 --USE Production_DB_name; -- Database dependent!
-SELECT OBJECT_NAME(object_id) AS TableName FROM sys.change_tracking_tables; -- Change tracking tables list
+SELECT OBJECT_NAME(object_id) AS TableName FROM sys.change_tracking_tables; -- Change tracked tables list
 
+select A.[name] as Table_name -- Change tracking tables
+	, (select top 1 [name] from sys.schemas where schema_id = A.schema_id) as Schema_
+	, OBJECT_NAME(A.parent_object_id) as Tracked_table
+	, A.create_date as Created
+	, A.modify_date as Modified
+	--, '-----' as [-----]
+	--, A.* 
+from sys.internal_tables A 
+where A.internal_type = 209
+;
 
-select * from sys.internal_tables where internal_type = 209; -- Change tracking tables
 exec sp_spaceused 'sys.change_tracking_356261120'; -- Space used by change tracking table
 exec sp_help 'sys.change_tracking_356261120'; -- Change tracking table structure 
 
