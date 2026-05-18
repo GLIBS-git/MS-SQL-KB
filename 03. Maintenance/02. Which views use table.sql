96221493b349a0085d7c862815bb04aabf2861ad
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-05-18 21:38:17 +0300

    Update 02. Which views use table.sql

--- 03. Maintenance/02. Which views use table.sql
+++ 03. Maintenance/02. Which views use table.sql
@@ -1,5 +1,5 @@
 select top 100 * from INFORMATION_SCHEMA.VIEWS -- Which views use table
-where VIEW_DEFINITION like '%Table_name%'
+where VIEW_DEFINITION like '%Table_name%' -- Table name here!
 ;
 
 
