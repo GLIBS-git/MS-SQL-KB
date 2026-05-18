d6fbdab5b918ded7c54610fa12cc000f184b06c6
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-05-18 21:37:51 +0300

    Create 03. Which stored procedures use table.sql

---
+++ 03. Maintenance/03. Which stored procedures use table.sql
@@ -0,0 +1,10 @@
+SELECT OBJECT_NAME(S.id) as Sp_name -- Which stored procedures use table
+FROM SYSCOMMENTS S
+INNER JOIN SYS.OBJECTS O ON O.Object_Id = S.id
+WHERE S.TEXT LIKE '%Table_name%' -- Table name here!
+AND O.type = 'p'
+order by Sp_name
+;
+
+
+
