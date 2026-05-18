a4fb9548c5eec830c6247734d5fc466383d1ce12
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-05-18 22:14:17 +0300

    Update 03. Which stored procedures use table.sql

--- 03. Maintenance/03. Which stored procedures use table.sql
+++ 03. Maintenance/03. Which stored procedures use table.sql
@@ -1,3 +1,5 @@
+--USE Production_DB_name; -- Database dependent!
+
 SELECT OBJECT_NAME(S.id) as Sp_name -- Which stored procedures use table
 FROM SYSCOMMENTS S
 INNER JOIN SYS.OBJECTS O ON O.Object_Id = S.id
