59eb71bfc71d1107f71b81e4a77ea2cb957de5ca
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-05-26 20:54:48 +0300

    Create 09.01 Locks detailed. View data.sql

---
+++ 01. State monitoring/09.01 Locks detailed. View data.sql
@@ -0,0 +1,14 @@
+
+
+SELECT %%lockres%% AS Lockres, '-----' as [-----], * FROM Table_name with (index(Index_name), nolock)
+where %%lockres%% in ('(4006394d3759)')
+;
+
+SELECT %%lockres%% AS Lockres, '-----' as [-----], * FROM Table_name with (index(Index_name))
+where %%lockres%% in ('(4006394d3759)')
+;
+
+
+
+
+
