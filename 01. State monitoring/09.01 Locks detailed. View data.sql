c49afe3a6026b804628f067a5565a76a28a358ae
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-05-27 11:12:01 +0300

    Update 09.01 Locks detailed. View data.sql

--- 01. State monitoring/09.01 Locks detailed. View data.sql
+++ 01. State monitoring/09.01 Locks detailed. View data.sql
@@ -1,4 +1,4 @@
-
+--USE Production_DB_name; -- Database dependent
 
 SELECT %%lockres%% AS Lockres, '-----' as [-----], * FROM Table_name with (index(Index_name), nolock)
 where %%lockres%% in ('(4006394d3759)')
