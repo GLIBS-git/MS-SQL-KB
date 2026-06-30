8781c08cecb6dc9d1886b61f1c82c8a38b8c9cff
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-06-30 17:40:11 +0300

    Update 03. Active transactions.sql

--- 01. State monitoring/03. Active transactions.sql
+++ 01. State monitoring/03. Active transactions.sql
@@ -51,6 +51,6 @@ SELECT -- Open transactions (+ duration)
 FROM sys.dm_tran_active_transactions A with (nolock)
 left join sys.dm_tran_database_transactions B with (nolock) on B.transaction_id = A.transaction_id
 -- One can adjust exemptions list for certain environment and tasks
-where A.name not in ('worktable','WorkFileGroup_fake_worktable','workfile','sort_fake_worktable','sort_init','LobStorageProviderSession','GhostCleanupTask','topn_fake_worktable')
+where A.name not in ('worktable','WorkFileGroup_fake_worktable','workfile','sort_fake_worktable','sort_init','LobStorageProviderSession','GhostCleanupTask','topn_fake_worktable','SplitPage')
 order by A.transaction_begin_time
 ;
