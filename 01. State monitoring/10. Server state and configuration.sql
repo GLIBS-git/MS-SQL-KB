5032dd2809605407bbfad62bdf29b541cc3e4fcd
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-07-06 20:53:21 +0300

    Update 10. Server state and configuration.sql

--- 01. State monitoring/10. Server state and configuration.sql
+++ 01. State monitoring/10. Server state and configuration.sql
@@ -1,9 +1,9 @@
 select A.sqlserver_start_time as Server_started -- Server start time and CPU/memory configuration
 	, A.cpu_count as Logical_CPU_cores
 	, A.hyperthread_ratio as Physical_CPU_cores
-	, A.physical_memory_kb as Physical_memory_Kb
-	, A.physical_memory_kb / 1024 as Physical_memory_Mb
 	, A.physical_memory_kb / 1024 / 1024 as Physical_memory_Gb
+	, A.physical_memory_kb / 1024 as Physical_memory_Mb
+	, A.physical_memory_kb as Physical_memory_Kb
 	, A.virtual_memory_kb as Virtual_memory_Kb
 	--, '-------' as [-------]
 	--, A.*
