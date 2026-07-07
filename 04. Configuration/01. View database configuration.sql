c580e70cb10642428332e322874ac42de6dd2393
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-07-07 20:13:19 +0300

    Update 01. View database configuration.sql

--- 04. Configuration/01. View database configuration.sql
+++ 04. Configuration/01. View database configuration.sql
@@ -1,4 +1,4 @@
-select A.[name] as Name_ -- DB configuration: snapshot & recovery state
+select A.[name] as Name_ -- DB configuration
 	, A.database_id as Id
 	, A.recovery_model_desc as Recovery_model
 	, case
@@ -7,10 +7,15 @@ select A.[name] as Name_ -- DB configuration: snapshot & recovery state
 		else '#Unknown#'
 	  end as Read_committed_snapshot_on
 	, A.snapshot_isolation_state_desc as Snapshot_isolation_state
+	, A.state_desc as Db_state
+	, A.is_auto_create_stats_on as Auto_create_stats
+	, A.is_auto_update_stats_on as Auto_update_stats
+	, A.collation_name as Collation
 	--, '-----' as [-----]
 	--, A.* 
 from sys.databases A
 ;
+
 
 
 
