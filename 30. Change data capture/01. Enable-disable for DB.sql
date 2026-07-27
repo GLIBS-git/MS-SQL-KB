54f2322181df60c7b5b8e79cd9bb43282227713e
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-07-27 14:27:21 +0300

    Update 01. Enable-disable for DB.sql

--- 30. Change data capture/01. Enable-disable for DB.sql
+++ 30. Change data capture/01. Enable-disable for DB.sql
@@ -1,6 +1,6 @@
 --USE Production_DB_name; -- Database dependent
 
-EXEC sys.sp_cdc_enable_db -- CDC enable for DB
+EXEC sys.sp_cdc_enable_db; -- CDC enable for DB
 
-EXEC sys.sp_cdc_disable_db -- CDC disable for DB
+EXEC sys.sp_cdc_disable_db; -- CDC disable for DB
 
