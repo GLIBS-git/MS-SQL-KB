a0483ac26440369a084b9a11b7dabbb4322dbcbb
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-07-31 11:41:48 +0300

    Update 03. Which stored procedures use table.sql

--- 03. Maintenance/03. Which stored procedures use table.sql
+++ 03. Maintenance/03. Which stored procedures use table.sql
@@ -1,12 +1,13 @@
 --USE Production_DB_name; -- Database dependent!
 
-SELECT OBJECT_NAME(S.id) as Sp_name -- Which stored procedures use table
+SELECT OBJECT_NAME(S.id) AS Sp_name -- Which stored procedures use table
 FROM SYSCOMMENTS S
 INNER JOIN SYS.OBJECTS O ON O.Object_Id = S.id
-WHERE S.TEXT LIKE '%Table_name%' -- Table name here!
+WHERE S.TEXT LIKE '%InventSize%' -- Table name here!
 AND O.type = 'p'
-order by Sp_name
+ORDER BY Sp_name
 ;
+
 
 
 
