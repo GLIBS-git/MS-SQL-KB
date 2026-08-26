41ca040926fb4201819d3fefc610b5ace17f9d6f
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-08-26 19:42:51 +0300

    Update 05. Column list for a table.sql

--- 10. Tunning/05. Column list for a table.sql
+++ 10. Tunning/05. Column list for a table.sql
@@ -1,6 +1,14 @@
+--USE Production_DB_name; -- Database dependent
+
 SELECT	A.name -- Columns for a table
 		, A.column_id 
 		, (SELECT [name] FROM sys.types WHERE system_type_id = A.system_type_id and user_type_id = A.user_type_id) Data_type
 FROM sys.columns A
 WHERE A.object_id = 1894124613 -- Filter on object id here
 ;
+
+
+
+
+
+
