1dfd5707a132962e507841a5c396bd75959c6bfd
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-08-26 19:58:48 +0300

    Update 02. CT setup help.sql

--- 31. Change tracking/02. CT setup help.sql
+++ 31. Change tracking/02. CT setup help.sql
@@ -3,20 +3,20 @@ SELECT DB_NAME(database_id) AS DatabaseName, is_auto_cleanup_on, retention_perio
 --USE Production_DB_name; -- Database dependent!
 SELECT OBJECT_NAME(object_id) AS TableName FROM sys.change_tracking_tables; -- Change tracked tables list
 
-select A.[name] as Table_name -- Change tracking tables
-	, A.[object_id] as [Object_id]
-	, (select top 1 [name] from sys.schemas where schema_id = A.schema_id) as Table_chema
-	, OBJECT_NAME(A.parent_object_id) as Tracking_table
-	, A.create_date as Created
-	, A.modify_date as Modified
-	--, '-----' as [-----]
+SELECT A.[name] AS Table_name -- Change tracking tables
+	, A.[object_id] AS [Object_id]
+	, (SELECT TOP 1 [name] FROM sys.schemas WHERE schema_id = A.schema_id) AS Table_chema
+	, OBJECT_NAME(A.parent_object_id) AS Tracking_table
+	, A.create_date AS Created
+	, A.modify_date AS Modified
+	--, '-----' AS [-----]
 	--, A.* 
-from sys.internal_tables A 
-where A.internal_type = 209
+FROM sys.internal_tables A 
+WHERE A.internal_type = 209
 ;
 
-exec sp_spaceused 'sys.change_tracking_356261120'; -- Space used by change tracking table
-exec sp_help 'sys.change_tracking_356261120'; -- Change tracking table structure. Scheme SYS is a must!
+EXEC sp_spaceused 'sys.change_tracking_356261120'; -- Space used by change tracking table
+EXEC sp_help 'sys.change_tracking_356261120'; -- Change tracking table structure.
 
 
 
