182579caa1436b3a6e233867b32c6bd4da02bb87
GLIBS-git <GLIBS@HOTMAIL.COM>
2026-08-26 15:24:16 +0300

    Update 01. DB tables size.sql

--- 02. Storage monitoring/01. DB tables size.sql
+++ 02. Storage monitoring/01. DB tables size.sql
@@ -1,23 +1,27 @@
 --USE Production_DB_name; -- Database dependent
 
-select	OBJECT_NAME(A.[object_id]) as Table_name -- DB tables size
-		, case when A.Index_id = 0 then 'HEAP' when A.Index_id = 1 then 'CLUSTERED' else '##ERROR##' end as Index_type
-		, A.Row_count as [Rows]
-		, (A.Reserved_data + A.Reserved_index) * 8 as Reserved_all_Kb
-		, A.Reserved_data * 8 as Reserved_data_Kb
-		, A.Reserved_index * 8 as Reserved_index_Kb
-		, case when Reserved_data <> 0 then round(CONVERT(real, A.Reserved_lob_data) / CONVERT(real, A.Reserved_data) * 100, 1) else 0 end as Reserved_lob_data_percent 
-		, case when Reserved_data <> 0 then round(CONVERT(real, A.Reserved_index) / CONVERT(real, A.Reserved_data) * 100, 1) else 0 end as Reserved_index_percent 
-		, case when A.Row_count <> 0 then round(CONVERT(real, A.Reserved_data) / CONVERT(real, A.Row_count) * 8 * 1024, 0) else 0 end as Average_row_size_bytes 
-from (
-	select	[object_id], MIN([index_id]) as Index_id, MAX(row_count) as Row_count
-			, SUM(case when [index_id] <= 1 then (in_row_reserved_page_count + lob_reserved_page_count + row_overflow_reserved_page_count) else 0 end) as Reserved_data
-			, SUM(case when [index_id] > 1 then (in_row_reserved_page_count + lob_reserved_page_count + row_overflow_reserved_page_count) else 0 end) as Reserved_index
-			, SUM(case when [index_id] <= 1 then lob_reserved_page_count else 0 end) as Reserved_lob_data
-	from sys.dm_db_partition_stats 
-	--where [object_id] in (OBJECT_ID('Some_table_name')) -- Some tables or comment for all tables
-	--where [object_id] in (1,2,3) -- Some table IDs or comment for all tables
-	group by [object_id]
+SELECT	OBJECT_NAME(A.[object_id]) AS Table_name -- DB tables size
+		, CASE WHEN A.Index_id = 0 THEN 'HEAP' WHEN A.Index_id = 1 THEN 'CLUSTERED' ELSE '##ERROR##' END AS Index_type
+		, A.Row_count AS [Rows]
+		, (A.Reserved_data + A.Reserved_index) * 8 AS Reserved_all_Kb
+		, A.Reserved_data * 8 AS Reserved_data_Kb
+		, A.Reserved_index * 8 AS Reserved_index_Kb
+		, CASE WHEN Reserved_data <> 0 THEN ROUND(CONVERT(real, A.Reserved_lob_data) / CONVERT(real, A.Reserved_data) * 100, 1) ELSE 0 END AS Reserved_lob_data_percent 
+		, CASE WHEN Reserved_data <> 0 THEN ROUND(CONVERT(real, A.Reserved_index) / CONVERT(real, A.Reserved_data) * 100, 1) ELSE 0 END AS Reserved_index_percent 
+		, CASE WHEN A.Row_count <> 0 THEN ROUND(CONVERT(real, A.Reserved_data) / CONVERT(real, A.Row_count) * 8 * 1024, 0) ELSE 0 END AS Average_row_size_bytes 
+FROM (
+	SELECT	[object_id], MIN([index_id]) AS Index_id, MAX(row_count) AS Row_count
+			, SUM(CASE WHEN [index_id] <= 1 THEN (in_row_reserved_page_count + lob_reserved_page_count + row_overflow_reserved_page_count) ELSE 0 END) AS Reserved_data
+			, SUM(CASE WHEN [index_id] > 1 THEN (in_row_reserved_page_count + lob_reserved_page_count + row_overflow_reserved_page_count) ELSE 0 END) AS Reserved_index
+			, SUM(CASE WHEN [index_id] <= 1 THEN lob_reserved_page_count ELSE 0 END) AS Reserved_lob_data
+	FROM sys.dm_db_partition_stats 
+	--WHERE [object_id] in (OBJECT_ID('Some_table_name')) -- Some tables or comment for all tables
+	--WHERE [object_id] in (1,2,3) -- Some table IDs or comment for all tables
+	GROUP BY [object_id]
 ) A
-order by Reserved_all_Kb desc
+ORDER BY Reserved_all_Kb DESC
 ;
+
+
+
+
